fe5a784c200fba28206f36d55b8b0d3cd8319d8c
rizwanali02 <rizwanali02hindoli@gmail.com>
2024-03-14 11:34:16 +0530

    test9

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chatapp-792n.onrender.com",
+        origin: "*",
         methods: ["GET, POST"]
     }
 });
